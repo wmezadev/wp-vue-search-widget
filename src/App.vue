1757293baab940d433b6759573737e6e4fd55fc2
William Eduardo Meza <williameduardomeza@gmail.com>
2019-05-24 20:29:16 -0500

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SearchComponent></SearchComponent>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SearchComponent from './components/SearchComponent.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    SearchComponent
   }
 }
 </script>
---
+++ src/components/SearchComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="row">
+        <div class="col col-md-4">
+          <label class="form-control"> Buscar </label>
+          <input type="text" name="search" placeholder="Búsqueda">
+        </div>
+        <div class="col col-md-4">
+          <label class="form-control"> Categoria </label>
+          <select name="category">
+            <option value=""> Una opción </option>
+          </select>
+        </div>
+        <div class="col col-md-4">
+          <button class="btn btn-primary">Buscar</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'search-component',
+  props: {
+    query: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
